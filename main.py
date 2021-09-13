b795e0e7a08f58589d25a34c83801e78cd00a012
beccakosey22 <820512@mystma.org>
2021-09-13 09:38:46 -0500

    Top four movies

---
+++ main.py
@@ -0,0 +1,11 @@
+#print first movie
+print("Twilight")
+
+#print second movie
+print("Moonrise Kingdom")
+
+#print third movie
+print("Portrait of a Lady on Fire")
+
+#print fourth movie
+print("Midsommar")
