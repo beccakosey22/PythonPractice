bc7a3912c844b091f2339dfcf23cfd36e9eac6f3
beccakosey22 <820512@mystma.org>
2021-09-15 09:11:30 -0500

    created three new fuctions

--- main.py
+++ main.py
@@ -15,5 +15,57 @@ def topFiveFilms():
     #print fifth movie
     print("Pulp Fiction")
 
+#print intro message
 print("These are my top five movies:")
 topFiveFilms()
+
+#define topFiveGames
+def topFiveGames():
+    #print first game
+    print("Stardew Valley")
+
+    #print second game
+    print("League of Legends")
+
+    #print third game
+    print("Rainbow Six Siege")
+
+    #print fourth game
+    print("Minecraft")
+
+    #print fifth game
+    print("Dead by Daylight")
+
+#define favoriteSchoolSubjects
+def favoriteSchoolSubjects():
+    #print first subject
+    print("Art")
+
+    #print second subject
+    print("TV Production")
+
+    #print third subject
+    print("History")
+
+    #print fourth subject
+    print("Math")
+
+    #print fifth subject
+    print("English")
+
+#define topFiveTelevisionShows
+def topFiveTelevisionShows():
+    #print first show
+    print("Parks and Rec")
+
+    #print second show
+    print("The Queen's Gambit")
+
+    #print third show
+    print("Brooklyn 99")
+
+    #print fourth show
+    print("The Vampire Diarys")
+
+    #print fifth show
+    print("Black Mirror")
