c2098d6f5c7c2811264faaa5c8b079d7e95c82e4
beccakosey22 <820512@mystma.org>
2021-09-27 09:48:57 -0500

    definition function

--- main.py
+++ main.py
@@ -1,111 +1,25 @@
-#define topFiveFilms
-def topFiveFilms():
-    #print first movie
-    print("Twilight")
-
-    #print second movie
-    print("Moonrise Kingdom")
-
-    #print third movie
-    print("Portrait of a Lady on Fire")
-
-    #print fourth movie
-    print("Midsommar")
-
-    #print fifth movie
-    print("Pulp Fiction")
-
-#print intro message
-print("These are my top five movies:")
-topFiveFilms()
-
-#define topFiveGames
-def topFiveGames():
-    #print first game
-    print("Stardew Valley")
-
-    #print second game
-    print("League of Legends")
-
-    #print third game
-    print("Rainbow Six Siege")
-
-    #print fourth game
-    print("Minecraft")
-
-    #print fifth game
-    print("Dead by Daylight")
-
-#define favoriteSchoolSubjects
-def favoriteSchoolSubjects():
-    #print first subject
-    print("Art")
-
-    #print second subject
-    print("TV Production")
-
-    #print third subject
-    print("History")
-
-    #print fourth subject
-    print("Math")
-
-    #print fifth subject
-    print("English")
-
-#define topFiveTelevisionShows
-def topFiveTelevisionShows():
-    #print first show
-    print("Parks and Rec")
-
-    #print second show
-    print("The Queen's Gambit")
-
-    #print third show
-    print("Brooklyn 99")
-
-    #print fourth show
-    print("The Vampire Diarys")
-
-    #print fifth show
-    print("Black Mirror")
-
-#define a function the outputs my name
-def myNameis(name):
-
-    #output string
-    print(name + " is my name")
-
-myNameis("Becca")
-
-#define function that multiplies input interger by 2
-def timesTwo(x):
-    print(x * 2)
-
-timesTwo(4)
-
-#define a function that adds 5 to input interger
-def plusFive(x):
-    print(x + 5)
-
-plusFive(2)
-
-#define function
-def divideByTwo(x):
-    print(x / 2)
-
-divideByTwo(4)
-
-#define function
-def addTwoNumbers(x, y):
-    print(x + y)
-
-addTwoNumbers(4, 6)
-
-#define a madlib function
-def madlib(nounOne, pluralNounOne, verbPresentTense, verbPresentTenseTwo, partOfBodyPlural, adjective, pluralNounTwo, adjectiveTwo):
-    print("Today, every student has a computer small enough to fit into his " + nounOne + ". He can solve any math problem by simply pushing the computer's little " + pluralNounOne + 
-    ". Computers can add, multiply, divide, and " + verbPresentTense + ". They can also " + verbPresentTenseTwo + " better than a human. Some computers are " + partOfBodyPlural + ". Others have a/an " + adjective + 
-    " screen that shows all kinds of " + pluralNounTwo + " and " + adjectiveTwo + " figures.")
-
-madlib("car", "signs", "run", "smell", "arms", "clean", "assignments", "small")
+#create function that gives definitions for ten words
+def define(word):
+    word = word.lower()
+    if word == "programming":
+        return "the process or activity of writing computer programs."
+    elif word == "computer":
+        return "an electronic device that processes and stores data"
+    elif word == "school":
+        return "any institution at which instruction is given in a particular discipline."
+    elif word == "IDE":
+        return "software for building applications that combines common developer tools into a single graphical user interface"
+    elif word == "keyboard":
+        return "a panel of keys that operate a computer or typewriter."
+    elif word == "teacher":
+        return "a person who teaches, especially in schools."
+    elif word == "lizard":
+        return "a reptile that typically has a long body and tail, four legs, movable eyelids, and a rough, scaly, or spiny skin."
+    elif word == "video game":
+        return "a game played by electronically manipulating images produced by a computer program on a television screen or other display screen."
+    elif word == "chameleon":
+        return "a small slow-moving Old World lizard with a prehensile tail, long extensible tongue, protruding eyes that rotate independently, and a highly developed ability to change color."
+    elif word == "fish":
+        return "a limbless cold-blooded vertebrate animal with gills and fins and living wholly in water."
+    else:
+        return word + " is not defined in this dictionary"
